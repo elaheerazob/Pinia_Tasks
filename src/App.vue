ada6ef3ca18f3c98830af2ab78fcd6fedf9797cd
elaheerazob <ridwanelahee282@gmail.com>
2023-11-27 23:49:25 +0600

    pinia Tasks vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <!-- Header -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="">
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
